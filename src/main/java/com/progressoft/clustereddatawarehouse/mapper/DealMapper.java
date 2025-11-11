e7b8dabf03fb2372af1fa35eb0894417fb2bdc29
MohammedFytahi <mohammedfytahi567@gmail.com>
2025-11-11 23:31:35 +0100

    feat: implement DTOs and mappers for Deal entity

---
+++ src/main/java/com/progressoft/clustereddatawarehouse/mapper/DealMapper.java
@@ -0,0 +1,19 @@
+package com.progressoft.clustereddatawarehouse.mapper;
+
+import com.progressoft.clustereddatawarehouse.dto.DealRequestDto;
+import com.progressoft.clustereddatawarehouse.dto.DealResponseDto;
+import com.progressoft.clustereddatawarehouse.model.Deal;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface DealMapper {
+
+    Deal toEntity(DealRequestDto dto);
+
+    DealResponseDto toDto(Deal entity);
+
+    List<DealResponseDto> toDtoList(List<Deal> entities);
+
+}
